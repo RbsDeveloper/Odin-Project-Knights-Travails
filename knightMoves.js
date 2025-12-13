9e5a1d7cd7d9bc9f8fcfb369d1e2aac080d9c4ee
Raul <developer.rbs@gmail.com>
2025-12-13 22:49:00 +0200

    Feat: Add initial BFS traversal helper skeleton

--- knightMoves.js
+++ knightMoves.js
@@ -2,3 +2,25 @@
 const knightMoves = (start, end) => {
     // Logic
 }
+
+const bfsTraversal = (startPoint) => {
+    const visited = {};
+    const queue = [startPoint];
+    const result = [];
+
+    visited[startPoint] = true;
+
+    let dequeuedVertex
+    while(queue.length) {
+        dequeuedVertex = queue.shift();
+        result.push(dequeuedVertex);
+        dequeuedVertex.forEach(element => {
+            if(!visited[element]){
+                visited[element] = true;
+                queue.push(element);
+            }
+        });
+    }
+    return result;
+
+}
