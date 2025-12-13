866bf1bb208aac7515dd58395d346be9803f6642
Raul <developer.rbs@gmail.com>
2025-12-13 23:51:08 +0200

    Feat: add knight neighbor generator helpers

--- knightMoves.js
+++ knightMoves.js
@@ -1,7 +1,44 @@
 
-const knightMoves = (start, end) => {
-    // Logic
+
+const moves = [
+    [+1, +2],
+    [+1, -2],
+    [-1, -2],
+    [-1, +2],
+    [+2, +1],
+    [+2, -1],
+    [-2, +1],
+    [-2, -1]
+]
+
+const inBounds = ([x,y]) => {
+    if(Number.isInteger(x) && Number.isInteger(y)){
+        if(x >= 0 && x < 8 && y>= 0 && y < 8){
+            return true
+        }else{
+            return false
+        }
+
+    }
+
+    return false
 }
+
+
+
+const findNeighbours = (vertex) => {
+    let neighbours = []
+    moves.forEach((move)=>{
+        let possibleNeighbour = [vertex[0]+move[0], vertex[1]+move[1]]
+        if(inBounds(possibleNeighbour)){
+            neighbours.push(possibleNeighbour)
+        }
+    })
+
+    return neighbours
+}
+
+console.log(findNeighbours([0,0]));
 
 const bfsTraversal = (startPoint) => {
     const visited = {};
@@ -23,4 +60,8 @@ const bfsTraversal = (startPoint) => {
     }
     return result;
 
+}
+
+const knightMoves = (start, end) => {
+    // Logic
 }
